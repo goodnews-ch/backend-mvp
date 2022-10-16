80018ffe76abb3268c4b6a4c11621963f26ca7c0
Siddharth Ganesan <siddharthganesan@gmail.com>
2022-10-16 05:20:50 -0700

    Fix cumulative data

---
+++ classification/categorize_text.py
@@ -0,0 +1,12 @@
+import cohere 
+from cohere.classify import Example 
+co = cohere.Client('3Gadb4V5oKd2YIwc6rz7Oxw6LOYFTxFSbg0nxy7k')
+
+def categorize_text(text_input):
+    response = co.classify(
+        model='99b88c18-5e7c-49f6-ab8c-cc57cdebb7a3-ft',
+        inputs=[text_input]
+    )
+    return response
+
+print(categorize_text("war"))
--- classification/classification_data/topics/concatenate_topics.py
+++ classification/classification_data/topics/concatenate_topics.py
@@ -7,22 +7,29 @@ DATAFRAMES = [
     ("Covid_and_Vaccine_News.csv", "COVID")
 ]
 
-def process():
-    dfs = []
-    for tup in DATAFRAMES:
-        dataframe, topic = tup
-        # Read raw data and remove neutral rows
-        df = pd.read_csv(dataframe, sep=',')
+# def process():
+#     dfs = []
+#     for tup in DATAFRAMES:
+#         dataframe, topic = tup
+#         # Read raw data and remove neutral rows
+#         df = pd.read_csv(dataframe, sep=',')
 
-        dfs.append(df)
-    
-    result = pd.concat(dfs)
-    result = result.drop("TITLE", 1)
-    result = result.sample(frac = 1)
-    result = result[~result['title'].isnull()]
-    
-    # Write processed data to new csv file
-    filename = "cumulative_data.csv"
-    result.to_csv(filename, index=False)
+#         dfs.append(df)
 
-process()
+#     result = pd.concat(dfs)
+#     # print(result)
+#     # result = result.drop("TITLE", 1)
+#     # result = result.sample(frac = 1)
+#     # result = result[~result['title'].isnull()]
+    
+#     # Write processed data to new csv file
+#     filename = "cumulative_data.csv"
+#     result.to_csv(filename, index=False)
+
+def shuffle():
+    df = pd.read_csv("cumulative_data.csv", sep=",")
+    df = df.sample(frac = 1)
+    df.to_csv("result.csv", index=False)
+
+
+shuffle()
